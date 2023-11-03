43ba13c631acae63c7e9b4ed6ddb02715e6761bb
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-03 23:06:30 +0100

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import "./globals.css";
+
+import type { Metadata, Viewport } from "next";
+import type { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export const metadata: Metadata = {
+  title: "Colemak Pro",
+  description: "Colemak keyboard trainer for professionals",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.svg",
+    },
+    {
+      rel: "mask-icon",
+      url: "/favicon.svg",
+      color: "#000000",
+    },
+    {
+      rel: "apple-touch-icon",
+      url: "/apple-touch-icon.png",
+    },
+  ],
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+};
