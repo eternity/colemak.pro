86ffad17c137392cae8a68fd3b0fac767cdff3e2
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-04 00:52:21 +0100

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+
   images: {
     domains: ["colemak.pro"],
+    deviceSizes: [
+      1280, // HD(720p)
+      1920, // Full HD(1080p)
+      2048, // 2K
+      3840, // 4K UHD
+    ],
+    remotePatterns: [
+      {
+        // Avatars of users signed in with Google
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.colemak.pro" }],
+        destination: "https://colemak.pro/:path*",
+        statusCode: 308,
+      },
+    ];
   },
 };
