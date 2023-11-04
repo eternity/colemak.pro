437e1fb1b2c3f880a791befb1fdf64b1a33f8eaf
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-04 21:33:18 +0100

    Upgrade next-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
-import type { AuthOptions } from "next-auth";
-
 import { FirestoreAdapter } from "@auth/firebase-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const nextAuthOptions: AuthOptions = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: FirestoreAdapter({ namingStrategy: "snake_case" }),
-  providers: [],
-};
+  providers: [GoogleProvider],
+});
