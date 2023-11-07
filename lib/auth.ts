e4b83249e1c2c6d45eca66b8249d3ca05f4f340f
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-07 07:46:17 +0100

    Set up SendGrid as email auth provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,20 @@
+import { sendEmail } from "@/lib/email";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: FirestoreAdapter({ namingStrategy: "snake_case" }),
-  providers: [GoogleProvider],
+  providers: [
+    GoogleProvider,
+    EmailProvider({
+      sendVerificationRequest: ({ identifier, url }) =>
+        sendEmail(
+          identifier,
+          "Sign in to your account",
+          `Here is the link to sign in to your account: ${url}`
+        ),
+    }),
+  ],
 });
