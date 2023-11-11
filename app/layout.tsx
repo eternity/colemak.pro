20a73cdfeea95bc38889ea6e7282461b62118440
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-11 08:21:57 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,18 @@ import "./globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
+import Script from "next/script";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EVF4JW4KED" />
+      <Script id="gtag">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-EVF4JW4KED');
+      `}</Script>
       <body>{children}</body>
     </html>
   );
