4dc038f59b9b1f8149e3355c41afaa24289ef774
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-17 13:27:40 +0100

    Set up CI/CD

--- lib/firebase/app.ts
+++ lib/firebase/app.ts
@@ -2,9 +2,10 @@ import { initializeApp } from "firebase/app";
 
 export const firebase = initializeApp({
   apiKey: "AIzaSyAsdCy9Qf8udvEN9tl16V_dRRGNOEFlVac",
+  appId: "1:885172774292:web:4a29dae37709dc0de05b81",
   authDomain: "outgoing-sneaky-quill.firebaseapp.com",
+  measurementId: "G-ZHKH2DEHPJ",
+  messagingSenderId: "885172774292",
   projectId: "outgoing-sneaky-quill",
   storageBucket: "outgoing-sneaky-quill.appspot.com",
-  messagingSenderId: "885172774292",
-  appId: "1:885172774292:web:4a29dae37709dc0de05b81",
 });
