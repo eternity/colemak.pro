e453331de731dd82bf6aa2e8fd2e5fa01f7277eb
Ilya Amursky <ilya.amursky@gmail.com>
2023-11-17 11:17:50 +0100

    Remove next-auth

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,12 +1,3 @@
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
-
 export default async function Page() {
-  const session = await auth();
-
-  if (!session?.user) {
-    redirect("/api/auth/signin");
-  }
-
   return <div>Account</div>;
 }
